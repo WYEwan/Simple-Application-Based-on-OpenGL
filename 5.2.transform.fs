88292022c7e5882b2b98e252826ac1f369eedf83
WYEwan <wangyiewan@foxmail.com>
2025-07-05 12:15:31 +0800

    Shader Program（vs+fs）

---
+++ 5.2.transform.fs
@@ -0,0 +1,17 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 Color;
+
+in vec2 TexCoord;
+
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    vec4 texColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+    FragColor = vec4(Color * texColor.rgb, texColor.a);
+}
